57c9d5837dce44f61ff9b17ac24db9307c09ac90
ScorpionOrange <scorpionorange@126.com>
2017-02-06 23:14:57 +0800

    2017.2.06: Testing in ipAddress mode, but not good enough.

--- src/main/java/PingDemo.java
+++ src/main/java/PingDemo.java
@@ -1,5 +1,13 @@
+import java.net.InetAddress;
+
 /**
  * Created by ScorpionOrange on 2017/02/06.
  */
 public class PingDemo {
+    public boolean ping(String ipAddress) throws Exception{
+        int timeOut = 3000;
+        //当返回值是true时，说明host是可用的，false则为不可
+        boolean status = InetAddress.getByName(ipAddress).isReachable(timeOut);
+        return status;
+    }
 }
--- src/main/java/PingDemoMain.java
+++ src/main/java/PingDemoMain.java
@@ -2,7 +2,10 @@
  * Created by ScorpionOrange on 2017/02/06.
  */
 public class PingDemoMain {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         System.out.println("Testing");
+        String Address = "www.baidu.com";
+        boolean is = new PingDemo().ping(Address);
+        System.out.println();
     }
 }
