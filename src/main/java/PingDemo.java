ae5473aefa3e0b524a2c384a6687433fa4573df9
ScorpionOrange <scorpionorange@126.com>
2017-02-06 21:08:08 +0800

    Initial commit

---
+++ src/main/java/PingDemo.java
@@ -0,0 +1,5 @@
+/**
+ * Created by ScorpionOrange on 2017/02/06.
+ */
+public class PingDemo {
+}
---
+++ src/main/java/PingDemoMain.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ScorpionOrange on 2017/02/06.
+ */
+public class PingDemoMain {
+    public static void main(String[] args){
+        System.out.println("Testing");
+    }
+}
