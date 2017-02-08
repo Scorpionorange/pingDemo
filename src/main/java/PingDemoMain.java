21fbd498236cc9491d1aa2d6442d0e0d220692e1
ScorpionOrange <scorpionorange@126.com>
2017-02-08 23:48:48 +0800

    2017.2.06: Testing in ipAddress mode, but not good enough.                 now try to do it again in HttpURLConnection methods.

--- src/main/java/PingDemoMain.java
+++ src/main/java/PingDemoMain.java
@@ -3,15 +3,17 @@
  */
 public class PingDemoMain {
     public static void main(String[] args) throws Exception {
-        //String url = "https://zhuanlan.zhihu.com/p/25070515";
+        String url = "https://zhuanlan.zhihu.com/p/25070515";
         //System.out.println("now testing: " + url);
         //System.out.println("Using Ping Method: " + new PingDemo().ping(url));
-        //System.out.println("Using Url Method: " + new PingDemo().isUrlExists(url));
+        System.out.println("Using Url Method: " + new PingDemo().isUrlExists(url));
 
+        /*
         String ApartUrl = "https://zhuanlan.zhihu.com/p/";
         for(long i = 25070000; i <= 25071000; i++){
             String BpartUrl = ApartUrl + i;
             System.out.println(BpartUrl + "   ///   " + new PingDemo().isUrlExists(BpartUrl));
         }
+        */
     }
 }
