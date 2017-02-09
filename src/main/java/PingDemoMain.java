096fcfe74fdcdb65621ff7d31208611f576a6031
ScorpionOrange <scorpionorange@126.com>
2017-02-09 23:47:03 +0800

    2017.2.06: Testing in ipAddress mode, but not good enough.                 now try to do it again in HttpURLConnection methods.

--- src/main/java/PingDemoMain.java
+++ src/main/java/PingDemoMain.java
@@ -1,9 +1,13 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 /**
  * Created by ScorpionOrange on 2017/02/06.
  */
 public class PingDemoMain {
     public static void main(String[] args) throws Exception {
-        String url = "https://zhuanlan.zhihu.com/p/25070515";
+        String url = "https://zhuanlan.zhihu.com/p/25070516";
         //System.out.println("now testing: " + url);
         //System.out.println("Using Ping Method: " + new PingDemo().ping(url));
         System.out.println("Using Url Method: " + new PingDemo().isUrlExists(url));
@@ -15,5 +19,38 @@ public class PingDemoMain {
             System.out.println(BpartUrl + "   ///   " + new PingDemo().isUrlExists(BpartUrl));
         }
         */
+        Document document = Jsoup.connect(url).get();
+        Elements elements = document.select("div.error-code");
+        String state = elements.text();
+        System.out.println(state);
+
+        /*
+        String url = "https://www.oschina.net/news";
+
+        Document document = Jsoup.connect(url).get();
+
+        Elements elements = document.select("#all-news");
+
+        for (Element element : elements) {
+            Elements titleElement = element.select("a[href]").select("class");
+            String title = titleElement.text();
+            String link = titleElement.attr("href").trim();
+
+            Elements dataElement = element.select(".date");
+            Elements authorElement = dataElement.select("a");
+            String author = authorElement.text();
+            authorElement.remove();
+            String date = dataElement.text();
+            String detail = element.select(".detail").text();
+
+            System.out.println("链接：        " + "http://www.oschina.net"+link);
+            System.out.println("标题：        " + title);
+            System.out.println("作者：        " + author);
+            System.out.println("发布时间： " + date);
+            System.out.println("详细信息： " + detail);
+            System.out.println();
+            System.out.println();
+        }
+         */
     }
 }
